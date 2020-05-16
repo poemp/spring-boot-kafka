5452d3d57a7af761c2adaaabef47dc69be61bf5f
lywwrking <xue_2013@sina.com>
2020-05-17 00:43:16 +0800

    修改，修改不需要的引用

--- src/main/java/org/poem/config/KafkaConsumerConfig.java
+++ src/main/java/org/poem/config/KafkaConsumerConfig.java
@@ -2,7 +2,6 @@ package org.poem.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.poem.listener.KafkaListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
